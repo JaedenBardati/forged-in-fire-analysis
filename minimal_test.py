eb6f0e03ffc18fed0df7cadce5126c0689da3115
Jaeden Bardati <jbardati@login1.frontera.tacc.utexas.edu>
2024-03-19 13:04:11 -0500

    removed useless print

--- minimal_test.py
+++ minimal_test.py
@@ -1,8 +1,6 @@
 import yt
 
 filepath = "/scratch3/01799/phopkins/fire3_suite_done/m13h206_m3e5/m13h206_m3e5_hyperref_5_done/output__collected/snapshot_334.hdf5"
-print('Attempting to load snapshot...')
-
 ds = yt.load(filepath)
 
 print('All done.')
