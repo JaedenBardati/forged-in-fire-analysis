a62295260440084e95852e983c1d01552cd186ee
Jaeden Bardati <jbardati@login3.frontera.tacc.utexas.edu>
2024-03-30 18:35:20 -0500

    drastically improved efficiency and accuracy

--- dust_structure.py
+++ dust_structure.py
@@ -3,59 +3,69 @@ import numpy as np
 import yt
 import matplotlib.pyplot as plt
 
-def integrate(integrand, variable, axis=None):
-    sorted_variable_args = np.argsort(variable)
-    sorted_integrand = integrand[sorted_variable_args]
-    sorted_variable = variable[sorted_variable_args]
-    return (sorted_integrand*sorted_variable).sum(axis=None)
 
 step = 334
 width = '1 kpc'
+
+L_1pc = np.array([-0.98523201,  0.14804156,  0.08603251])
+""" Code to get L_1pc:
+snap = an.load_fifs_box(step=step, width='1 pc')
+L_1pc = snap.gas_angular_momentum
+an.LOGGER.info('1 pc angular momentum is: {}'.format(L_1pc))
+"""
+
 snap = an.load_fifs_box(step=step, width=width)
 
+# get quanities
 pos = snap.dust_centered_pos.in_units('pc')
 rho = snap[('Dust', 'density')].in_units('g/cm**3')
 
-## to do : rotate to angular momentum
-
+# convert to cylindrical coords
 x, y, z = pos.T
-s, phi, z = an.cart_to_cyl(x, y, z)
+s, phi, z = an.cart_to_cyl(x, y, z, normal=L_1pc)
 
-N = 1000
-zlimit_1, zlimit_2 = -0.1, 0.1
+# sort by phi for quick integration
+sorted_phi_args = np.argsort(phi)
+sorted_rho = rho[sorted_phi_args]
+sorted_s = s[sorted_phi_args]
+sorted_phi = phi[sorted_phi_args]
+sorted_z = z[sorted_phi_args]
+
+N = 100
+zlimit_1, zlimit_2 = -0.25, 0.25
 logslimit_1, logslimit_2 = -3, 3
 ZS = np.linspace(zlimit_1, zlimit_2, num=N+1)
 SS = np.logspace(logslimit_1, logslimit_2, num=N+1)
-DZS = np.diff(ZS)
-DSS = np.diff(SS)
 
 an.LOGGER.info('Computing density map ...')
 
 avg_rho_map = np.nan * np.ones((N, N))
+z_slice = [np.logical_and(ZS[i] <= sorted_z, sorted_z < ZS[i+1]) for i in range(N)]
+s_slice = [np.logical_and(SS[j] <= sorted_s, sorted_s < SS[j+1]) for j in range(N)]
 for i in range(N):  # y: z
-    Z1, Z2, DZ = ZS[i], ZS[i+1], DZS[i]
-    an.LOGGER.info(' >> Row %d : %.4f < z < %.4f' % (i, Z1, Z2))
-    z_limit = np.logical_and(Z1 <= z, z < Z2)
-
+    an.LOGGER.info(' >> Row %d/%d : %.4f < z < %.4f' % (i+1, N, ZS[i], ZS[i+1]))
     for j in range(N):  # x: s
-        S1, S2, SZ = SS[j], SS[j+1], DSS[j]
+        slice_ij = np.logical_and(z_slice[i], s_slice[j])
+        avg_rho_map[i, j] = an.integral_average(sorted_rho[slice_ij], sorted_phi[slice_ij], weights=sorted_s[slice_ij], already_sorted=True).in_units('g/cm**3')
 
-        s_limit = np.logical_and(S1 <= s, s < S2)
-        sub_slice = np.logical_and(z_limit, s_limit)
-
-        sub_rho = rho[sub_slice]
-        sub_s = s[sub_slice]
-        sub_phi = phi[sub_slice]
-
-        avg_rho_map[i, j] = (integrate(sub_rho*sub_s, sub_phi)/integrate(sub_s, sub_phi)).in_units('g/cm**3')
-
-an.LOGGER.info('Making figure ...')
+an.LOGGER.info('Making figure(s) ...')
 
 plt.figure()
 im = plt.imshow(avg_rho_map, extent=(logslimit_1, logslimit_2, zlimit_1, zlimit_2), aspect='auto', cmap='hot')
-plt.colorbar(im, label=r'Average mass density $\langle\rho\rangle$ [g/cm$^3$]')
+plt.colorbar(im, label=r'Average dust mass density $\langle\rho\rangle$ [g/cm$^3$]')
 plt.xlabel(r'log Cylindrical radius $s$ [pc]')
 plt.ylabel(r'Height $z$')
 plt.savefig('avg_rho_map_cyl_sz.pdf')
+plt.clf()
+
+plt.figure()
+im = plt.imshow(np.log10(avg_rho_map), extent=(logslimit_1, logslimit_2, zlimit_1, zlimit_2), aspect='auto', cmap='hot')
+plt.colorbar(im, label=r'log Average dust mass density $\langle\rho\rangle$ [g/cm$^3$]')
+plt.xlabel(r'log Cylindrical radius $s$ [pc]')
+plt.ylabel(r'Height $z$')
+plt.savefig('avg_rho_map_cyl_sz_log.pdf')
+plt.clf()
+
+an.LOGGER.info('done.')
 
 ## TO DO: Use this to visualize at blackbody emission location better
